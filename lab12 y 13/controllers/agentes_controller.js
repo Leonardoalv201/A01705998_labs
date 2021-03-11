79e98a7194a513bd7078180e3bb2ec66145be63e
Leonardo Alvarado Menendez <A01705998@itesm.mx>
2021-03-10 20:53:24 -0600

    log de cookie

--- lab12 y 13/controllers/agentes_controller.js
+++ lab12 y 13/controllers/agentes_controller.js
@@ -9,10 +9,16 @@ exports.getNuevoAgente = (request,response,next)=>{
 exports.postNuevoAgente = (request,response,next)=>{
     const nuevo_agente=new Agente(request.body.guardar_agente, request.body.imagen_agente)
     nuevo_agente.save();
+
+    response.setHeader('Set-Cookie', ['ultimo_agente='+ nuevo_agente.agente]);
+
     response.redirect('/agentes');
 }
 
 exports.get = (request,response,next)=>{
+    
+    console.log(request.get('Cookie').split(';')[1].trim().split('=')[1]);
+    
     response.render('agentes', {
         lista_agentes: Agente.fetchAll(),
         Titulo:"Agentes"
--- lab12 y 13/controllers/armas_controller.js
+++ lab12 y 13/controllers/armas_controller.js
@@ -9,10 +9,16 @@ exports.getNuevoArma = (request,response,next)=>{
 exports.postNuevoArma = (request,response,next)=>{
     const nuevo_arma=new Arma(request.body.guardar_arma, request.body.imagen_arma)
     nuevo_arma.save();
+
+    response.setHeader('Set-Cookie', ['ultima_arma='+ nuevo_arma.arma]);
+
     response.redirect('/armas');
 }
 
 exports.get = (request,response,next)=>{
+
+    console.log(request.get('Cookie').split(';')[1].trim().split('=')[1]);
+    
     response.render('armas', {
         lista_armas: Arma.fetchAll(),
         Titulo:"Armas"
